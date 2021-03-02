54e655ac75cf94fa387bde67d9733e454c9b060c
Cyber2f08 <79893882+Cyber2f08@users.noreply.github.com>
2021-03-02 18:23:03 +0700

    Module Banner⚙
    For Customizing...

---
+++ mdl/banner.py
@@ -0,0 +1,13 @@
+import pyfiglet
+import random
+import colorama
+from colorama import Fore, Back, Style
+from termcolor import colored
+colorama.init(autoreset=True)
+
+ascii_banner = pyfiglet.figlet_format("Dice me UP!")
+print(colored(ascii_banner, 'green'))
+print(colored( "|? /github/UnknownKID/dice/  UnknownKID, Cyber2f08", 'blue'))
+print(colored("----------------------> DICE <---------------------\n--------------------> Cyber2f08 <------------------", 'red'))
+print(colored( "{ Contact : cyber.2f08@dnmx.org\n{ Phone : +6288803212354\n{ Github : 'Soon'\n---------------------->{+};{msg = 'Hello there!'}|", 'green'))
+
